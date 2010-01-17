3dd0a2b4d89826b7326f019556f2fc8c39841ab6
Keiji, Yoshimi <walf443@gmail.com>
2010-01-17 19:27:57 +0900

    initial commit

---
+++ lib/DBIx/Skinny/ProxyTable.pm
@@ -0,0 +1,33 @@
+package DBIx::Skinny::ProxyTable;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Skinny::ProxyTable -
+
+=head1 SYNOPSIS
+
+  use DBIx::Skinny::ProxyTable;
+
+=head1 DESCRIPTION
+
+DBIx::Skinny::ProxyTable is
+
+=head1 AUTHOR
+
+Keiji Yoshimi E<lt>walf443 at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
