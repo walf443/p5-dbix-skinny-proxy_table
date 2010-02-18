05d0c9fd54fc773cb44cf7f96ba2ce54183459e5
Keiji, Yoshimi <walf443@gmail.com>
2010-02-18 22:53:28 +0900

    you can run test with MySQL using ENV.

--- t/Mock/Basic.pm
+++ t/Mock/Basic.pm
@@ -5,10 +5,21 @@ use DBIx::Skinny setup => +{
     password => '',
 };
 
+# you can run test with MYSQL if set env SKINNY_MYSQL_DSN.
+my ($dsn, $username, $password) = @ENV{map { "SKINNY_MYSQL_${_}" } qw/DSN USER PASS/};
+if ( $dsn ) {
+    __PACKAGE__->connect_info(+{dsn => $dsn, username => $username, password => $password });
+}
+
 use DBIx::Skinny::Mixin modules => [qw(ProxyTable)];
 
 sub setup_test_db {
     my $self = shift;
+    for my $table ( qw/ access_log access_log_201001 access_log_201002 ranking ranking_daily/ ) {
+        $self->do(sprintf(q{
+            DROP TABLE IF EXISTS %s;
+        }, $table));
+    }
     $self->do(q{
         CREATE TABLE access_log (
             id   INT,
