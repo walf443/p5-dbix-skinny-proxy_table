33b6107cf5de1d07f0a36bceba22da96a93bfd15
Keiji, Yoshimi <walf443@gmail.com>
2011-02-03 23:48:25 +0900

    Checking in changes prior to tagging of version 0.09.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e1c96df..14b89aa 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension DBIx::Skinny::ProxyTable
    
    +0.09    Thu Feb  3 23:47:28 JST 2011
    +        - follow DBIx::Skinny 0.0732
    +
     0.08    Tue Dec 21 08:33:32 JST 2010
             - minor fix: test dependencies.

--- lib/DBIx/Skinny/ProxyTable.pm
+++ lib/DBIx/Skinny/ProxyTable.pm
@@ -1,7 +1,7 @@
 package DBIx::Skinny::ProxyTable;
 use strict;
 use warnings;
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 use DBIx::Skinny::ProxyTable::Rule;
 use Carp;
 use DBIx::Skinny::Util qw();
